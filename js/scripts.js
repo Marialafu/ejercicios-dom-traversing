51a53cd65cd203aa45a78cb7a460ff8a5da64d10
Marialafu <lafuentelhmaria@gmail.com>
2025-04-01 11:59:22 +0200

    actualizado

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,38 @@ console.log(
   subtitle2Element.parentElement.classList[0],
   subtitle2Element.parentElement.children[2].textContent
 );
+
+console.clear();
+
+// - Crea un h1 con el texto "Soy un título ", al ponerte con el ratón encima del h1, deberá cambiar al texto a "Quita de encima!!!" y al quitarte, deberá volver a poner "Soy un título"
+
+const titleElement = document.getElementById('title');
+
+const changeText = () => {
+  titleElement.textContent = 'Quita de encima!!!';
+};
+const rechangeText = () => {
+  titleElement.textContent = 'Soy un título';
+};
+
+titleElement.addEventListener('mouseover', changeText);
+titleElement.addEventListener('mouseout', rechangeText);
+
+// - Crea un botón con la etiqueta <button> en tu página HTML con el texto "Click me" y añadele un evento de click. Al hacer click tendrá que poner "clicked x times" donde x será el número de veces que hiciste click en él.
+
+const clickMeButtonElement = document.getElementById('click-me-button');
+let counterClicks = 0;
+
+const countClicks = () => {
+  counterClicks++;
+
+  clickMeButtonElement.textContent = `Cliked ${counterClicks} times`;
+};
+
+console.dir(clickMeButtonElement);
+
+clickMeButtonElement.addEventListener('click', countClicks);
+
+// - Crea un p con el texto "esperando entrada de teclado..." al pulsar cualquier tecla deberá poner "has pulsado la tecla tal" y al soltarla el <p> volverá a tener el texto "esperando entrada de teclado...". Como reto extra puedes intentar añadir si se ha usado una combinación de teclas con alt, shift o control.
+
+// - Crea un array con 5 palabras, las que tú quieras. Añade un h2 a tu HTML. Añade dos botones con el texto previous y next respectívamente. Haz que los botones cambien el texto del h2 con las palabras del array, cuando llegues a la última volverás a la primera al pulsar el botón next y cuando estés en la primera podrás volver a la última haciendo click al botón previous.
